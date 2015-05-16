b7fec17922928c213b3a80c86ce6bad78bb9564a
kaitlinmuth <kaitlin.muth@gmail.com>
2015-05-16 00:20:59 -0500

    Initial commit

---
+++ routes/register.js
@@ -0,0 +1,23 @@
+/**
+ * Created by kaitlinmuth on 5/15/15.
+ */
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+var Users = require('../models/user');
+
+router.get('/', function(req, res, next){
+    res.sendFile(path.resolve(__dirname, '../views/register.html'));
+});
+
+router.post('/', function(req,res,next) {
+    Users.create(req.body, function (err, post) {
+        if (err)
+            next(err);
+        else
+            res.redirect('/users');
+    })
+});
+
+module.exports = router;
